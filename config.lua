6b59e26b9854ee046012d85216aaeeb22ff3592f
Zhang Huanjie <james@ustc.edu.cn>
2018-07-14 13:11:15 +0800

    aprs

--- config.lua
+++ config.lua
@@ -1,22 +1,23 @@
---[[
-config.lua | Tiest van Gool
-
-Global variables definitions for usage across various lua scripts
---]]
-
 --wifi module
 wifi_ssid = "ustcnet"
 wifi_password = ""
-http_url = "http://202.38.64.40/upload_temp_humi.php"
-temp_humi = ""
-count = 0
 
-FLOAT_FIRMWARE = (1/3) > 0
+send_http = true
+http_host = "202.38.64.40"
+http_url = "/upload_temp_humi.php"
+
+send_count_interval = 10
+
+send_aprs = true
+aprs_server = "202.141.176.2"
+aprs_port = 14580
+aprs_prefix = "BG6CQ-15>ES66:=3150.59N/11716.00E_"
+
 
 --dht module
 dht_pin = 2  -- Pin for DHT22 sensor (GPIO4)
 -- dht_pin = 3 -- GPIO0
 -- dht_pin = 4 -- GPIO2
 
--- Status Message
 print("Global variables loaded")
+
--- dht22.lua
+++ dht22.lua
@@ -7,50 +7,61 @@ Once connection is established dht module and temperature and humidity is retrie
 -- Load global user-defined variables
 dofile("config.lua")
 
+temp_humi = ""
+count = 0
+
 -- Connect to the wifi network using wifi module
 wifi.setmode(wifi.STATION)
 wifi.sta.config({ssid=wifi_ssid, pwd=wifi_password})
 wifi.sta.autoconnect(1)
 wifi.sta.connect()
 
+function send_data(temp, humi) 
+  print("My IP is "..wifi.sta.getip())
+  if (send_aprs) then
+      print("try send to aprs "..aprs_server)
+      str=aprs_prefix.."000/000g000t"..string.format("%03d", temp*9/5+32).."r000p000h"..string.format("%02d",humi).."b00000".."ESP8266 MAC "..wifi.sta.getmac()
+      print(str)
+      conn=net.createUDPSocket() 
+      conn:send(aprs_port,aprs_server,str) 
+      conn:close()
+      print("send ok")
+  end
+  if (send_http) then      
+      req_url= http_url.."?mac="..wifi.sta.getmac().."&"..string.format("temp=%.1f&humi=%.1f",temp,humi)
+      print("try send to http://"..http_host..req_url)
+      conn=net.createConnection(net.TCP, 0) 
+      conn:on("receive", function(conn, payload) print(payload) end)
+      conn:connect(80,host) 
+      conn:send("GET "..req_url.." HTTP/1.1\r\n") 
+      conn:send("Host: "..http_host.."\r\n") 
+      conn:send("Accept: */*\r\n") 
+      conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
+      conn:send("\r\n")
+      conn:on("sent",function(conn)
+        print("send ok")
+        conn:close()
+        end)
+      conn:on("disconnection", function(conn)
+        print("Got disconnection...")
+        end)
+  end
+end
 -- Read out DHT22 sensor using dht module
 function func_read_dht()
   print ("starting read")
   status, temp, humi, temp_dec, humi_dec = dht.read(dht_pin)
   if( status == dht.OK ) then
-    if FLOAT_FIRMWARE then
-      temp_humi = string.format("temp=%.1f&humi=%.1f",temp,humi)
-    else
-      temp_humi = string.format("temp=%d.%03d&humi=%d.%03d",temp, temp_dec, humi, humi_dec)
-    end
+    temp_humi = string.format("temp=%.1f, humi=%.1f",temp,humi)
     print(temp_humi)
     count = count + 1
-    if( count == 10 ) then
+    if( count == send_count_interval ) then
       count = 0
-      print("try to send to "..http_url)
       if wifi.sta.status() == 5 then  --STA_GOTIP
-         print("Connected to "..wifi.sta.getip())
-         url= "?mac=" .. wifi.sta.getmac().. "&" .. temp_humi
-         print(url)
-         print("Sending data to 202.38.64.40")
-         conn=net.createConnection(net.TCP, 0) 
-         conn:on("receive", function(conn, payload) print(payload) end)
-         conn:connect(80,'202.38.64.40') 
-         conn:send("GET /send_temp_humi.php"..url.." HTTP/1.1\r\n") 
-         conn:send("Host: 202.38.64.40\r\n") 
-         conn:send("Accept: */*\r\n") 
-         conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
-         conn:send("\r\n")
-         conn:on("sent",function(conn)
-           print("Closing connection")
-           conn:close()
-           end)
-         conn:on("disconnection", function(conn)
-           print("Got disconnection...")
-           end)
+         send_data(temp, humi)
       else
          print("wifi still connecting...")
-      end
+      end   
     end
   elseif( dht_status == dht.ERROR_CHECKSUM ) then          
     print( "DHT Checksum error" )
@@ -60,3 +71,4 @@ function func_read_dht()
 end
 
 tmr.alarm(1,2500,tmr.ALARM_AUTO,func_read_dht)
+
